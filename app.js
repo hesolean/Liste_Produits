44b9d131558c2697334053c9c999233a7561cd74
unknown <lndubourg@gmail.com>
2023-08-31 15:33:36 +0200

    mise en place des méthodes pour faire circuler les information des enfants vers les parents

--- app.js
+++ app.js
@@ -9,33 +9,129 @@ const PRODUCTS = [
   ];
 
 class SearcheBar extends React.Component {
+    constructor(props) {
+        super(props)
+        this.handelFilterTextChange = this.handelFilterTextChange.bind(this)
+        this.handelInStockChange = this.handelInStockChange.bind(this)
 
+    }
+
+    handelFilterTextChange(e) {
+        // on appelle la méthode du composant parent pour passer l'information
+        this.props.onFilterChange(e.target.value)
+    }
+
+    handelInStockChange(e) {
+        // on appelle la méthode du composant parent pour passer l'information
+        this.props.onInStockOnlyChange(e.target.checked)
+    }
+
+    render() {
+        const {filterText, inStockOnly} = this.props
+        return <div>
+            <div className="form-group">
+                <input 
+                    type="text" 
+                    value={filterText} 
+                    placeholder="Rechercher" 
+                    onChange={this.handelFilterTextChange}
+                />
+                
+            </div>
+            <div className="form-check" >
+                <input 
+                    type="checkbox" 
+                    checked={inStockOnly} 
+                    className="form-check-input" 
+                    id="stock"
+                    onChange={this.handelInStockChange}
+                />
+                <label htmlFor="stock" className="form-group-label">
+                    Ne montrer que les produits en stock
+                </label>
+            </div>
+        </div>
+    }
 }
 
 function ProductTable({products}) {
+    const rows = []
+    // on piste la dernière catégorie rencontrée
+    let lastCategory = null
+
+    // on fait une boucle pour passer en revue tous les produits en mettant
+    // une condition sur la catégorie pour changer de groupe
+
+    products.forEach((product => {
+        if (product.category !== lastCategory) {
+            lastCategory = product.category
+            // à chaque nouvelle categorie, je pousse le nom de la categorie
+            rows.push(<ProductCategoryRow key={lastCategory} category={lastCategory} />)
+        }
+        // puis je pousse chaque ligne de la même catégorie
+        rows.push(<ProductRow key={product.name} product={product} />)
+    }))
     return <table className="table">
-
-    </table>
+                <thead>
+                    <tr>
+                        <th scope="col">Nom des produits</th>
+                        <th scope="col">Prix ($)</th>
+                    </tr>
+                </thead>
+                <tbody>{rows}</tbody>
+            </table>    
 }
 
-class ProductCategoryRow extends React.Component {
-
+function ProductCategoryRow({category}) {
+    return <tr>
+        <th colSpan="2">{category}</th>
+    </tr>
 }
 
-class ProductRow extends React.Component {
-    
+function ProductRow({product}) {
+    const name = product.stocked ? 
+        product.name : 
+        // si on n'utilise pas bootstrp, on remplace par style={{color: "red"}}
+        <span className="text-danger">{product.name}</span>
+    return <tr>
+            <td>{name}</td>
+            <td>{product.price}</td>
+        </tr>
 }
+        
 
 class FilterableProducTable extends React.Component {
 constructor(props) {
     super(props)
+    this.state = {
+        filterText: "Foot",
+        inStockOnly: false
+    }
+    this.handelFilterTextChange = this.handelFilterTextChange.bind(this)
+    this.handelInStockChange = this.handelInStockChange.bind(this)
+
+}
+
+handelFilterTextChange(filterText){
+    this.setState({filterText})
+}
+
+handelInStockChange(inStockOnly){
+    this.setState({inStockOnly})
 }
 
 render() {
     const {products} = this.props
-    return <div>
-        {JSON.stringify(products)}
-    </div>
+    return <React.Fragment>
+        {JSON.stringify(this.state)}
+        <SearcheBar 
+            filterText={this.state.filterText}
+            inStockOnly={this.state.inStockOnly} 
+            onFilterTextChange={this.handelFilterTextChange} 
+            onInStockOnlyChange={this.handelInStockChange}
+        />
+        <ProductTable products={products}/>
+    </React.Fragment>
 }
 }
 
