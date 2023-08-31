4b59547db87744157ffa31afd711c581d69b101e
unknown <lndubourg@gmail.com>
2023-08-31 12:42:47 +0200

    injection de la table de produits dans le composant principal

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-function Home () {
-    return <h1>Hello</h1>
+// on crée la table de données
+const PRODUCTS = [
+    {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+    {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+    {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+    {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+    {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+    {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+  ];
+
+function ProductTable({products}) {
+    return <table className="table">
+        
+    </table>
 }
-ReactDOM.render(<Home />, document.querySelector("#app"))
+
+class FilterableProducTable extends React.Component {
+constructor(props) {
+    super(props)
+}
+
+render() {
+    const {products} = this.props
+    return <div>
+        {JSON.stringify(products)}
+    </div>
+}
+}
+
+ReactDOM.render(<FilterableProducTable products={PRODUCTS} />, document.querySelector("#app"))
